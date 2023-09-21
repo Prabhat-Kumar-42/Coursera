adc5e965c327f5ce6f3637e4e7b9be347d303235
blaze <prabhatkumarverma42@gmail.com>
2023-09-21 14:31:13 +0530

    Fixed Issue #0003

--- Algorithms_Part_1/week_1/UnionFind/Percolation.java
+++ Algorithms_Part_1/week_1/UnionFind/Percolation.java
@@ -1,11 +1,10 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Percolation {
-  
   private WeightedQuickUnionUF unionFindObj;
   private int [][]grid;
   private int size;
@@ -46,28 +45,28 @@ public class Percolation {
       // return;
       throw new IllegalArgumentException();
     }
-    System.out.println("Opening grid[" + row + "][" + col +"]"); 
+    // System.out.println("Opening grid[" + row + "][" + col +"]"); 
     boolean isFullFlag = false;
     grid[row][col] = OPEN;
     openSites++;
     if(row == 1) {
       isFullFlag = true;
       unionFindObj.union(0, size*(row-1) + col );
-      System.out.println("row = 1, Assigned 0 as parent, Parent is : " + 
-                          unionFindObj.find(size*(row-1)+col));
+      // System.out.println("row = 1, Assigned 0 as parent, Parent is : " + 
+      //                    unionFindObj.find(size*(row-1)+col));
     }
     else if(row == size-1)
     {
       unionFindObj.union(size*size - 1, size*(row-1) + col);
-      System.out.println("row = size-1, " + (size - 1)+ " Assigned " + 
-                          (size*size-1) + " size*size-1 as parent, Parent is : " + 
-                          unionFindObj.find(size*(row-1)+col));
+      // System.out.println("row = size-1, " + (size - 1)+ " Assigned " + 
+      //                    (size*size-1) + " size*size-1 as parent, Parent is : " + 
+      //                    unionFindObj.find(size*(row-1)+col));
       // System.out.println("Assigned Parent, Parent is : " + 
       //                    unionFindObj.find(size*(row-1) + col));
     }
-    System.out.println("Current Parent of grid[" + row + "][" + col + "] = "
-                        + unionFindObj.find(size*(row-1) + col)); 
-    System.out.println("Neighbours");
+    // System.out.println("Current Parent of grid[" + row + "][" + col + "] = "
+    //                     + unionFindObj.find(size*(row-1) + col)); 
+    // System.out.println("Neighbours");
     for(int i = 0; i < 4; i++) {
       int neighourRow = row + yCordinates[i];
       int neighourCol = col + xCordinates[i];
@@ -76,22 +75,22 @@ public class Percolation {
         continue;
       }
       if(isOpen(neighourRow, neighourCol)) {
-         System.out.println("Checking isFull of neighbour[" + neighourRow + "]["+
-                            neighourCol + "] = " + isFull(neighourRow, neighourCol));
+        // System.out.println("Checking isFull of neighbour[" + neighourRow + "]["+
+        //                    neighourCol + "] = " + isFull(neighourRow, neighourCol));
           
         if(isFullFlag && !isFull(neighourRow, neighourCol)) {
           unionFindObj.union(size*(row-1) + col, size*(neighourRow-1) + neighourCol);
-          System.out.println("Assigning current as neighbour parent");
-          System.out.println("current parent = " + unionFindObj.find(size*(row-1) + col));
-          System.out.println("Assigning current as neighbour parent");
+        //  System.out.println("Assigning current as neighbour parent");
+        //  System.out.println("current parent = " + unionFindObj.find(size*(row-1) + col));
+        //  System.out.println("Assigning current as neighbour parent");
         } else {
           unionFindObj.union(size*(neighourRow-1) + neighourCol, size*(row-1) + col);
-          System.out.println("Assigning neighbour as parent");
+        //  System.out.println("Assigning neighbour as parent");
         }
-        System.out.println("current parent = " + unionFindObj.find(size*(row-1) + col));
+        // System.out.println("current parent = " + unionFindObj.find(size*(row-1) + col));
       }
     }
-    System.out.println("Neighbours end");
+    // System.out.println("Neighbours end");
   }
 
   public boolean isOpen(int row, int col) {
@@ -101,8 +100,8 @@ public class Percolation {
       throw new IllegalArgumentException();
     }
     // System.out.println((grid[row][col] == OPEN));
-    System.out.println("Checking isOpen for grid[" + row + "][" + col +"] is " +
-                        (grid[row][col] == OPEN));
+    // System.out.println("Checking isOpen for grid[" + row + "][" + col +"] is " +
+    //                    (grid[row][col] == OPEN));
     return grid[row][col] == OPEN;
   }
 
@@ -113,9 +112,9 @@ public class Percolation {
       throw new IllegalArgumentException();
     }
 
-    System.out.println("In isFull, parent of grid[" + row + "][" + col + "] is " + 
-                        unionFindObj.find(size*(row-1) + col));
-    return unionFindObj.find((row-1)*size + col) == 0;
+    // System.out.println("In isFull, parent of grid[" + row + "][" + col + "] is " + 
+    //                    unionFindObj.find(size*(row-1) + col));
+    return unionFindObj.find((row-1)*size + col) == unionFindObj.find(0);
   }
 
   public int numberOfOpenSites() {
@@ -123,17 +122,17 @@ public class Percolation {
   }
 
   public boolean percolates() {
-    if(unionFindObj.find(0) == unionFindObj.find(size*size - 1)) {
-      System.out.println("Parent of 0 is " + unionFindObj.find(0));
-      System.out.println("Parent of " + (size*size-1) + " is " + unionFindObj.find(size*size-1));
-    }
+    // if(unionFindObj.find(0) == unionFindObj.find(size*size - 1)) {
+    //   System.out.println("Parent of 0 is " + unionFindObj.find(0));
+    //  System.out.println("Parent of " + (size*size-1) + " is " + unionFindObj.find(size*size-1));
+    // }
     return unionFindObj.find(0) == unionFindObj.find(size*size - 1);
   }
-
+  /*
   public static void main(String[] args) {
     int row, col;
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter size");
+    // System.out.println("Enter size");
     Percolation obj = new Percolation(sc.nextInt());
     while(!obj.percolates())
     {
@@ -143,6 +142,7 @@ public class Percolation {
       }while(obj.isOpen(row, col));
       obj.open(row, col);
     }
-    System.out.println(obj.percolates());
+    // System.out.println(obj.percolates());
   }
+  */
 }
